e3d5505e249d41846237b47be5d3040f1f41c647
Morten Olav Hansen <mortenoh@gmail.com>
2013-12-01 22:27:19 +0100

    minor fix

--- js/services/OrganisationUnitResource.js
+++
@@ -1,9 +0,0 @@
-
-fredApp.factory('OrganisationUnitResource', ['$resource', function( $resource ) {
-  return $resource(baseUrl + '/api/organisationUnits/:id', {'id': '@id'}, {
-    query: {
-      method: 'GET',
-      isArray: false
-    }
-  });
-}]);
